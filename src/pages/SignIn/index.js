f1dcbe9612dbfcaf20a4a43b1b5dc8349a15af2a
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2021-07-25 17:15:28 -0300

    Signin page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,38 @@
+
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './signin.css';
+import logo from '../../assets/logo.png';
+
 
 function SignIn(){
-    return(
-      <div>
-        <h1>Pagina de Login</h1>
-      </div>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert("ok")
   }
+
+  return(
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value)}/>
+          <input type="password" placeholder="senha" value={password} onChange={ (e) => setPassword(e.target.value)}/>
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+
+      </div>
+    </div>
+  );
+}
   
-  export default SignIn;
+export default SignIn;
