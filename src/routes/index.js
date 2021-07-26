af56efded998b75471909a0966860a4efed81052
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2021-07-25 22:14:20 -0300

    Pages with routes activeted

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,24 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
 
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import Customers from '../pages/Customers';
+import New from '../pages/New';
 
 export default function Routes(){
-    return(
-        <Switch>
-            <Route exact path="/" component={SignIn}/>
-            <Route exact path="/register" component={SignUp}/>
-            <Route exact path="/dashboard" component={Dashboard} isPrivate/>
-        </Switch>
-    )
+  return(
+    <Switch>
+      <Route exact path="/" component={SignIn} />
+      <Route exact path="/register" component={SignUp} />
+
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
+      <Route exact path="/profile" component={Profile} isPrivate />
+      <Route exact path="/customers" component={Customers} isPrivate />
+      <Route exact path="/new" component={New} isPrivate />
+      <Route exact path="/new/:id" component={New} isPrivate />
+      
+    </Switch>
+  )
 }
